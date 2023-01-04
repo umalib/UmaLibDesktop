cb3f03950123a0c301bccbde87774f4106693b64
WindyWisper <umalib629@outlook.com>
2023-01-04 18:03:24 +0800

    build: update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,18 @@ module.exports = {
   pluginOptions: {
     electronBuilder: {
       builderOptions: {
+        appId: 'cn.umafan.lib',
+        productName: '赛马娘同人集中楼大书库',
+        mac: {
+          icon: 'icons/icon.icns',
+          artifactName: 'umalib-mac-v${version}.${ext}',
+          target: ['dmg'],
+        },
+        win: {
+          icon: 'icons/256x256.png',
+          artifactName: 'umalib-win64-${version}.${ext}',
+          target: [{ target: '7z', arch: ['x64'] }],
+        },
         extraResources: [
           'prisma/**/*',
           'node_modules/.prisma/**/*',
