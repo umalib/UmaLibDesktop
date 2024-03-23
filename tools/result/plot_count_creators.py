dd80599c1ce30bb61abd09561bbce61c0bbebdae
WindyWhisper <108405915+umalib@users.noreply.github.com>
2024-03-23 16:28:16 +0800

    test: updates tools

--- tools/result/plot_count_creators.py
+++ tools/result/plot_count_creators.py
@@ -20,7 +20,7 @@ config = {
 }
 rcParams.update(config)
 
-ticksX = [data['days'][0], 18809, 18901, 18993, 19083, 19174, 19266, 19358, 19448, 19539, 19631, data['days'][-1]]
+ticksX = [data['days'][0], 18809, 18901, 18993, 19083, 19174, 19266, 19358, 19448, 19539, 19631, 19723, data['days'][-1]]
 
 creatorLabels = data['dict']['creators']
 
--- tools/result/plot_count_delta.py
+++ tools/result/plot_count_delta.py
@@ -19,7 +19,7 @@ config = {
 }
 rcParams.update(config)
 
-ticksX = [data['days'][0], 18809, 18901, 18993, 19083, 19174, 19266, 19358, 19448, 19539, 19631, data['days'][-1]]
+ticksX = [data['days'][0], 18809, 18901, 18993, 19083, 19174, 19266, 19358, 19448, 19539, 19631, 19723, data['days'][-1]]
 ticksY = list(np.arange(0, max(data['delta']), 10))
 
 exceptionVal = round(data['all'][-1] * 4 / len(data['all']))
