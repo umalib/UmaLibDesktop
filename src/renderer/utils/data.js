1a44e95683ec63d618f56440f050a72f9df2b702
WindyWhisper <108405915+umalib@users.noreply.github.com>
2024-04-03 10:08:15 +0800

    feat: updates signatures

--- src/renderer/utils/data.js
+++ src/renderer/utils/data.js
@@ -40,11 +40,11 @@ module.exports = {
   editors: editorList,
   elTagTypes: ['info', '', 'warning', 'success', 'danger'],
   signInfo: {
-    content: '内容：NGA赛马娘翻译交流群 | 开发：风之低吟 | 版本：3.0.0-Final',
+    content: '内容：NGA赛马娘翻译交流群 | 开发：风之低吟 | 版本：3.0.1-Final',
     pubKey:
       '04f7c5d1bf43e06c4a119deb999c33a488fc38d1a7f6387cdc0001ed190d6b304846b3d2931fb15f819c6e57ac7ce119f8c68e376a5631d5ccfc1f712a51187123',
     sign:
-      '3045022100c2ecb6ccc89e1997b0f570a653acc0d65f4f923640274eb023092f8bfbf5915202207a781316d6010b985fee6934344bc28d69f074516a4fa12936118894110a5e4d',
+      '304402202de93abc0d269e05ce72b52bcac0797cb12e44821c097e2c0d31836358a34c5202207e253dba1e7abf0a37244b2b55829cd9a9bee64ffa1db62af521f19e99df55f8',
   },
   staffs: staffList,
   tagTypes: ['其他', '角色', '系列', '长篇/合集', '争议/不适'],
